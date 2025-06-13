819f70e34da684dac9561f2db4768ce49e21106b
regis <regisbatistadossantos25@gmail.com>
2025-06-13 02:50:10 -0300

    recomeço

--- upgrades/index.js
+++ upgrades/index.js
@@ -8,9 +8,14 @@ function clicou(){
     filho.innerHTML = 'Clicou';
     filho.style.borderColor = 'deepskyblue';
 }   //Função de quando o mouse clica na caixa
-function saiu() {
-    filho.style.background = 'red'
-    filho.innerHTML = 'Clique'
-    filho.style.borderColor = 'brown'
+function saiu() {  
+    filho.style.background = 'green'
+    filho.innerHTML = 'Saiu'
+    filho.style.borderColor = 'dark green'
+    setTimeout(() => {
+        filho.style.background = 'red'
+        filho.innerHTML = 'Clique'
+        filho.style.borderColor = 'brown'
+    }, 3000);   // tempo em miléssimos (3000ms = 3s)
     
-}   //função de quando o mouse sai da caixa
+}   //função de quando o mouse sai da caixa e um timer de 3 segundos
